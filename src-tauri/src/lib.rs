d1f0cc8bb9a9523449fd410eb568e9b4cccce74c
SharmaDevanshu089 <sharma.devanshu0089@gmail.com>
2025-12-04 17:43:15 +0530

    [STABLE] Working in Acrylic

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+use window_vibrancy::apply_acrylic;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +9,14 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+
+            // ⭐ Apply acrylic effect
+            apply_acrylic(&window, None).expect("Failed to apply acrylic");
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use window_vibrancy::apply_acrylic;
 fn main() {
     bg_blur_test_lib::run()
 }
